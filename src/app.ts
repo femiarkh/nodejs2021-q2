35e3558adffd169fb458a42c4e7a8b653df9efab
Dmitry Gutkovsky <femiarkh@gmail.com>
2021-06-04 07:48:05 +0300

    feat: handle unexisting routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 import path from 'path';
 import YAML from 'yamljs';
+import { StatusCodes } from 'http-status-codes';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
@@ -27,5 +28,12 @@ app.use('/', (req, res, next) => {
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 boardRouter.use('/:boardId/tasks', taskRouter);
+
+app.all('*', (req, res) =>
+  res.status(StatusCodes.NOT_FOUND).json({
+    status: 'fail',
+    message: `Sorry, can't find ${req.originalUrl}`,
+  })
+);
 
 module.exports = app;
--- src/middlewares/loggingMiddleware.ts
+++ src/middlewares/loggingMiddleware.ts
@@ -9,15 +9,24 @@ const loggingMiddleware = (req: Request, res: Response, next: NextFunction) => {
   finished(res, () => {
     const ms = Date.now() - start;
     const { statusCode } = res;
-    const log = `${method} ${url} ${statusCode} [${ms}ms]\nquery parameters: ${JSON.stringify(
-      query
-    )}\nbody: ${JSON.stringify(body)}`;
+    const log = `[${new Date().toLocaleString()}]
+${method} ${url} ${statusCode} [${ms}ms]
+query parameters: ${JSON.stringify(query)}
+request body: ${JSON.stringify(body)}\n`;
     console.log(log);
-    fs.appendFile('logs.txt', `${log}\n\n`, (err) => {
+    fs.appendFile('logs.main.txt', `${log}\n`, (err) => {
       if (err) {
         console.log(err);
       }
     });
+    // log errors in edition to main logging file
+    if (!res.statusCode.toString().startsWith('2')) {
+      fs.appendFile('logs.errors.txt', `${log}\n`, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
   });
 };
 
