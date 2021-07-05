bad89445ac3bacd65e52aaa44591fc7b7c687535
Dmitry Gutkovsky <femiarkh@gmail.com>
2021-07-05 07:34:55 +0300

    feat: add login functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  ForbiddenException,
+  Post,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 import { UserService } from 'src/user/user.service';
 
-@Controller('auth')
+@Controller()
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  @Post('login')
+  async login(
+    @Body('login') login: string,
+    @Body('password') password: string,
+  ) {
+    const user = await this.userService.findOne({ login });
+
+    if (!user) {
+      throw new ForbiddenException('User not found');
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    const jwt = await this.jwtService.signAsync({
+      userId: user.id,
+      login: user.login,
+    });
+
+    return {
+      token: jwt,
+    };
+  }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
+import 'dotenv/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: process.env.SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
