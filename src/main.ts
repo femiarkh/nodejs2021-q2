fca3426ef6a43dd418dca5be3fca04b73614031e
Dmitry Gutkovsky <femiarkh@gmail.com>
2021-07-11 14:40:25 +0300

    doc: add swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
 import { NestApplication, NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
@@ -7,6 +9,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { MyLogger } from './utils/logger.service';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   let app: NestApplication | NestFastifyApplication;
@@ -20,8 +23,13 @@ async function bootstrap() {
   } else {
     app = await NestFactory.create(AppModule, { logger: true });
   }
-
   app.useGlobalPipes(new ValidationPipe());
+
+  const document = yaml.load(
+    fs.readFileSync('doc/api.yaml', 'utf8'),
+  ) as OpenAPIObject;
+  SwaggerModule.setup('doc', app, document);
+
   app.useLogger(app.get(MyLogger));
   await app.listen(process.env.PORT, '0.0.0.0');
   console.log(`listening on port ${process.env.PORT}`);
--- src/utils/http-exception.filter.ts
+++ src/utils/http-exception.filter.ts
@@ -5,22 +5,28 @@ import {
   HttpException,
   Injectable,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
+import 'dotenv/config';
 
 @Injectable()
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
     const status = exception.getStatus();
     const res = exception.getResponse();
 
-    response.status(status).json({
+    const payload = {
       timestamp: new Date().toISOString(),
       path: request.url,
       response: res,
-    });
+    };
+
+    if (process.env.USE_FASTIFY === 'true') {
+      response.code(status).send(payload);
+    } else {
+      response.status(status).json(payload);
+    }
   }
 }
